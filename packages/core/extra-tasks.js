eda44e851cf6f72c4d3db4e25d88639a29cabaa4
dimaslz <me@dimaslz.io>
2021-11-27 01:54:07 +0100

    fix(input-text): add input text component to compile task

--- packages/core/extra-tasks.js
+++ packages/core/extra-tasks.js
@@ -5,65 +5,75 @@ const shelljs = require('shelljs');
 const nodeModulesDir = path.resolve(`${__dirname}/node_modules`);
 
 function renameCssVarPrefix(css, prefix) {
-	return css.replace(/--tw-/g, `--${prefix}-`);
+  return css.replace(/--tw-/g, `--${prefix}-`);
 }
 
 function cleanCSS(css) {
-	return css.replace('@tailwind utilities; ', '');
+  return css.replace('@tailwind utilities; ', '');
 }
 
 function moveSCSS() {
-	shelljs.mkdir('./dist/scss');
-	shelljs.mkdir('./dist/scss/components');
+  shelljs.mkdir('./dist/scss');
+  shelljs.mkdir('./dist/scss/components');
 
-	shelljs.cp('./src/assets/css/variables.scss', './dist/scss/variables.scss');
-	shelljs.cp('./src/components/button/button.scss', './dist/scss/components/button.scss');
-	fs.writeFileSync(
-		path.resolve(`${__dirname}/dist/scss/components/components.scss`),
-		`@import '~tailwindcss/utilities.css';
-@import 'button.scss';`,
-	);
+  shelljs.cp('./src/assets/css/variables.scss', './dist/scss/variables.scss');
+  shelljs.cp(
+    './src/components/button/button.scss',
+    './dist/scss/components/button.scss',
+  );
+  shelljs.cp(
+    './src/components/input-text/input-text.scss',
+    './dist/scss/components/input-text.scss',
+  );
+  fs.writeFileSync(
+    path.resolve(`${__dirname}/dist/scss/components/components.scss`),
+    `@import '~tailwindcss/utilities.css';
+@import 'button.scss';
+@import 'intput-text.scss';`,
+  );
 }
 
 function getVariables(css) {
-	const originalUtilitiesCSS = fs.readFileSync(
-		path.resolve(`${nodeModulesDir}/tailwindcss/dist/utilities.css`),
-		'utf8'
-		);
+  const originalUtilitiesCSS = fs.readFileSync(
+    path.resolve(`${nodeModulesDir}/tailwindcss/dist/utilities.css`),
+    'utf8',
+  );
 
-	let match = originalUtilitiesCSS.match(new RegExp('\\*\\s*\\{[^\\}]+\\}', 'gm'));
-	if (match) {
-		let variables = match.join('\n')
-		variables = variables
-			.replace(/\n/gm, '')
-			.replace(/\s+--/gm, '--')
-			.replace(/\s+\{/gm, '{')
-			.replace(/\s+/gm, ' ');
+  let match = originalUtilitiesCSS.match(
+    new RegExp('\\*\\s*\\{[^\\}]+\\}', 'gm'),
+  );
+  if (match) {
+    let variables = match.join('\n');
+    variables = variables
+      .replace(/\n/gm, '')
+      .replace(/\s+--/gm, '--')
+      .replace(/\s+\{/gm, '{')
+      .replace(/\s+/gm, ' ');
 
-		return `${variables} ${css}`;
-	}
+    return `${variables} ${css}`;
+  }
 
-	return css;
+  return css;
 }
 
 async function run() {
-	console.log("👨‍💻 Making some last fixes...")
-	const cssOrigin = fs.readFileSync(
-		path.resolve(`${__dirname}/dist/cross-ui/cross-ui.css`),
-		'utf8'
-	);
+  console.log('👨‍💻 Making some last fixes...');
+  const cssOrigin = fs.readFileSync(
+    path.resolve(`${__dirname}/dist/cross-ui/cross-ui.css`),
+    'utf8',
+  );
 
-	let cssUpdated = cssOrigin;
-	cssUpdated = getVariables(cssUpdated);
-	cssUpdated = renameCssVarPrefix(cssUpdated, 'cui');
-	cssUpdated = cleanCSS(cssUpdated);
+  let cssUpdated = cssOrigin;
+  cssUpdated = getVariables(cssUpdated);
+  cssUpdated = renameCssVarPrefix(cssUpdated, 'cui');
+  cssUpdated = cleanCSS(cssUpdated);
 
-	fs.writeFileSync(
-		path.resolve(`${__dirname}/dist/cross-ui/cross-ui.css`),
-		cssUpdated,
-	);
+  fs.writeFileSync(
+    path.resolve(`${__dirname}/dist/cross-ui/cross-ui.css`),
+    cssUpdated,
+  );
 
-	moveSCSS();
+  moveSCSS();
 }
 
 run();
